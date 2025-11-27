6290660a1ac77a8a2fc136d0c69b93f0391038b1
Masshh <msipicyna96@gmail.com>
2025-11-28 00:51:13 +0500

    добавлен класс Client на сервере

---
+++ Server/Classes/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Classes
+{
+    public class Client
+    {
+        public string Token { get; set; }
+        public string Login { get; set; }
+        public DateTime DateConnect { get; set; }
+        public Client()
+        {
+            Random random = new Random();
+            string Chars = "FDSAFASGfdsfGSDFGSDFGFDSF12345678988884441111";
+            this.Token = new string(Enumerable.Repeat(Chars, 15).Select(x => x[random.Next(Chars.Length)]).ToArray());
+            DateConnect = DateTime.Now;
+        }
+    }
+}

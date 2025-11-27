6f4a92666a90e695efa03d22e23033d3546fa896
Masshh <msipicyna96@gmail.com>
2025-11-28 01:07:50 +0500

    добавлен контекст БД

---
+++ Server/Classes/DbContexted.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Classes
+{
+    public class DbContexted : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(
+                "server=127.0.0.1;port=3306;database=license;user=root;password=",
+                new MySqlServerVersion(new Version(8, 0)));
+        }
+    }
+}

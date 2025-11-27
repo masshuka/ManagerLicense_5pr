2619c04d40b407b926958d96c644ed58fb39f9a3
Masshh <msipicyna96@gmail.com>
2025-11-28 00:59:57 +0500

    Добавлен класс User на сервере

---
+++ Server/Classes/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
